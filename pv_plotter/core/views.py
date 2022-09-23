2da16ac6b1c917d9ab59e7fba57ab46a92b0398c
Jhon Luengas <jeluengasm@unal.edu.co>
2022-09-23 12:36:20 -0500

    feat(home): Urls, templates schemmas and views finished

--- pv_plotter/core/views.py
+++ pv_plotter/core/views.py
@@ -4,3 +4,47 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'core/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class LoginView(TemplateView):
+    template_name = 'core/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class RegisterView(TemplateView):
+    template_name = 'core/register.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class ForgotPasswordView(TemplateView):
+    template_name = 'core/forgot_password.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class RestorePasswordView(TemplateView):
+    template_name = 'core/restore_password.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class PVPlotView(TemplateView):
+    template_name = 'core/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
